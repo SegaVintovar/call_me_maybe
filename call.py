844ebfdfb359e93994aef184dd0889337981d433
Valentyn Sudak <vsudak@f0r1s12.codam.nl>
2026-05-29 17:19:35 +0200

    prompt engeneering is done

--- call.py
+++ call.py
@@ -14,17 +14,22 @@ class Tokenvalidation():
 
 
 def generate_text(
-        model: Small_LLM_Model, prompt_text, max_new_tokens=64) -> str:
+        model: Small_LLM_Model, prompt_text, max_new_tokens=128) -> str:
 
-    input_ids = model.encode(prompt_text)
+    input_ids = model.encode(prompt_text)[0].tolist()
     prompt_l = len(input_ids)
+    start_ids = model.encode(ai_help(prompt_text))[0].tolist()
+    input_ids += start_ids
     print(input_ids)
     # [0].tolist()
     for _ in range(max_new_tokens):
         logits = model.get_logits_from_input_ids(input_ids)
         # here i need to pick a valid logit,
         # not the one with highest probability
-        next_token_id = max(range(len(logits)), key=lambda i: logits[i])
+        # next_token_id = max(range(len(logits)), key=lambda i: logits[i])
+        # print(logits[:10])
+        max_logit = max(logits)
+        next_token_id = logits.index(max_logit)
 
         input_ids.append(next_token_id)
 
@@ -32,31 +37,29 @@ def generate_text(
 
 
 def build_function_call_prompt(functions, user_prompt):
-    return f"""
-                You are a function selection engine.
-                to answer users prompt,
-                choose exactly one function from the list below.
+    return f'You are a function selection engine.\
+To answer users prompt, choose exactly one function from the list below.\
+Available functions:{functions}\n\
+User prompt: {user_prompt}' +\
+        r'Answer only in valid JSON format.\
+Follow the schema below:\n\
+[\
+    {\
+    "prompt": <user_prompt>,\
+    "name": <function_name>,\
+    "parameters": {"param_1": value}\
+    }\
+]'
+                    # ,\
+                    # {\
+                    # "prompt": "Reverse the string "hello"",\
+                    # "name": "fn_reverse_string",\
+                    # "parameters": {"s": "hello"}\
+                    # }\
 
-                Available functions:
-                {functions}
 
-                Return only valid JSON in this format:
-                [
-                    {
-                    "prompt": "What is the sum of 2 and 3?",
-                    "name": "fn_add_numbers",
-                    "parameters": {"a": 2.0, "b": 3.0}
-                    },
-                    {
-                    "prompt": "Reverse the string 'hello'",
-                    "name": "fn_reverse_string",
-                    "parameters": {"s": "hello"}
-                    }
-                ]
-
-                User prompt:
-                {user_prompt}
-                """
+def ai_help(fn_name: str) -> str:
+    return r"[{'prompt':" + fn_name
 
 
 s_llm = Small_LLM_Model()
@@ -71,7 +74,7 @@ try:
         # prompts = usr_input.read()
         # print("prompts before json load\n", prompts, type(prompts))
         prompts_json = json.load(usr_input)
-        print("prompts after json load\n", prompts, type(prompts))
+        print("prompts after json load\n", prompts_json, type(prompts_json))
     with open(s_llm.get_path_to_vocab_file()) as v:
         # vocab = v.read()
         vocab = json.load(v)
@@ -88,8 +91,11 @@ except Exception as e:
 
 function_names: list = [p["name"] for p in funs]
 prompts_list = [p["prompt"] for p in prompts_json]
-generate_text(s_llm, prompts_list[0], )
-
+print(prompts_list[4])
+p = build_function_call_prompt(funs, prompts_list[4])
+print(p)
+text = generate_text(s_llm, p)
+print(text)
 # vocab = vocab.strip(r"{}")
 # vocab_list = vocab.split(",")
 
